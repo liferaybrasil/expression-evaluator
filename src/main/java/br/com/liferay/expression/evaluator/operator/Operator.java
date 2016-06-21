eb92823a685298c8a0aa8091d3ba5d876c194b4a
Leonardo Barros <leonardo.barros@liferay.com>
2016-06-21 09:05:31 -0300

    Implement equals and hashCode

--- src/main/java/br/com/liferay/expression/evaluator/operator/Operator.java
+++ src/main/java/br/com/liferay/expression/evaluator/operator/Operator.java
@@ -61,6 +61,31 @@ public class Operator implements Comparable<Operator> {
 		return null;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((text == null) ? 0 : text.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Operator other = (Operator) obj;
+		if (text == null) {
+			if (other.text != null)
+				return false;
+		} else if (!text.equals(other.text))
+			return false;
+		return true;
+	}
+
 	public int compareTo(Operator operator) {
         return this.getPrecedence() - operator.getPrecedence();
     }
